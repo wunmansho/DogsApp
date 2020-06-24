023f92347c93ba159639c1d2be29ae7092a455a2
wunmansho <wunmansho.wunmansho@gmail.com>
2020-06-24 11:15:08 -0400

    Lesson 54

---
+++ app/src/main/java/com/rogers/dogsapp/model/DogPalette.java
@@ -0,0 +1,9 @@
+package com.rogers.dogsapp.model;
+
+public class DogPalette {
+    public int color;
+
+    public DogPalette(int color) {
+        this.color = color;
+    }
+}
--- app/src/main/java/com/rogers/dogsapp/view/DetailFragment.java
+++ app/src/main/java/com/rogers/dogsapp/view/DetailFragment.java
@@ -1,32 +1,30 @@
 package com.rogers.dogsapp.view;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModel;
+
 import androidx.lifecycle.ViewModelProviders;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
+import androidx.palette.graphics.Palette;
+
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.rogers.dogsapp.R;
 import com.rogers.dogsapp.databinding.FragmentDetailBinding;
 import com.rogers.dogsapp.model.DogBreed;
-import com.rogers.dogsapp.util.Util;
+import com.rogers.dogsapp.model.DogPalette;
 import com.rogers.dogsapp.viewmodel.DetailViewModel;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class DetailFragment extends Fragment {
 
@@ -65,10 +63,38 @@ public class DetailFragment extends Fragment {
         viewModel.dogLiveData.observe(this, dogBreed -> {
             if (dogBreed != null && dogBreed instanceof DogBreed && getContext() != null) {
                 binding.setDog(dogBreed);
+                if(dogBreed.imageUri != null) {
+                    setupBackgroundColor(dogBreed.imageUri);
+                }
 
             }
 
         });
+    }
+
+    private void setupBackgroundColor(String url)  {
+        Glide.with(this)
+                .asBitmap()
+                .load(url)
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        Palette.from(resource)
+                                .generate(palette -> {
+                                    int intColor = palette.getLightMutedSwatch().getRgb();
+                                    DogPalette myPalette = new DogPalette(intColor);
+                                    binding.setPalette(myPalette);
+
+                                });
+
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+                });
+
     }
 
 
